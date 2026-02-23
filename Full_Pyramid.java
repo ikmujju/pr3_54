5a24fa0c72ccb114efff500c9e3d58e609f96bce
Tirth-me <gothitirth@gmail.com>
2026-02-24 00:07:46 +0530

    Updated pyramid patterns

--- Full_Pyramid.java
+++ Full_Pyramid.java
@@ -1,16 +1,22 @@
-public class Full_Pyramid
-{
-    public static void main(String[] args)
-    {
-        for (int i = 1; i <= 5; i++)
-        {
-            for (int j = 1; j <= 5 - i; j++)
-            {
+public class Full_Pyramid {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
                 System.out.print(" ");
             }
-            for (int k = 1; k <= i; k++)
-            {
+            for (int k = 1; k <= i; k++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+            char ch = 'A';
+            for (int k = 1; k <= i; k++) {
+                System.out.print(ch + " ");
+                ch++;
             }
             System.out.println();
         }
--- Pyramid.java
+++ Pyramid.java
@@ -1,6 +1,23 @@
 public class Pyramid {
     public static void main(String[] args) {
-        int rows = 6; // You can change the number of rows if needed
+        int rows = 5;
+        int num = 1;
+
+        // Number part
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(num + " ");
+                num++;
+
+                if (num == 10)   // reset after 9
+                {
+                    num = 1;
+                }
+            }
+            System.out.println();
+        }
 
         for (int i = rows; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
