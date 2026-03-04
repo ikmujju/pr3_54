f872b4e214cb56b4b0c70cfa9984e409475e57e2
Tirth-me <gothitirth@gmail.com>
2026-03-04 20:36:28 +0530

    Modify

--- Full_Pyramid.java
+++ Full_Pyramid.java
@@ -1,7 +1,8 @@
 public class Full_Pyramid {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5 - i; j++) {
+        int a = 5;
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= a - i; j++) {
                 System.out.print(" ");
             }
             for (int k = 1; k <= i; k++) {
@@ -9,8 +10,8 @@ public class Full_Pyramid {
             }
             System.out.println();
         }
-        for (int i = 5; i >= 1; i--) {
-            for (int j = 1; j <= 5 - i; j++) {
+        for (int i = a; i >= 1; i--) {
+            for (int j = 1; j <= a - i; j++) {
                 System.out.print(" ");
             }
             char ch = 'A';
